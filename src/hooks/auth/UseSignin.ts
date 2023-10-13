ffa09a1bc0e32d2fc27ff49ade1e059fc99dd89d
Ghassen Saaf <saafghassen@gmail.com>
2023-10-13 17:08:01 +0100

    dep fix

--- src/hooks/auth/UseSignin.ts
+++ src/hooks/auth/UseSignin.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { useDispatch } from 'react-redux';
-import { gql, useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../../features/auth/authSlice';
 import { SIGN_IN } from '../../utils/GraphQL/Mutations';
