397192f72fe2ff283258b30433975da71c98be27
Ghassen Saaf <saafghassen@gmail.com>
2023-11-26 13:45:13 +0100

    commit

--- src/pages/errors/NotFound.tsx
+++ src/pages/errors/NotFound.tsx
@@ -3,12 +3,9 @@
 // src/CoolNotFound.tsx
 import React from 'react';
 import { Button, Container, Typography } from '@mui/material';
-import { useSpring, animated } from 'react-spring';
 import { Link } from 'react-router-dom';
 
 const CoolNotFound: React.FC = () => {
-  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
-
   return (
     <Container
       style={{
@@ -19,17 +16,15 @@ const CoolNotFound: React.FC = () => {
         height: '80vh',
       }}
     >
-      <animated.div style={props}>
-        <Typography variant="h1" color="textPrimary" gutterBottom>
-          404 Not Found
-        </Typography>
-        <Typography variant="h4" color="textSecondary" paragraph>
-          Oops, it looks like you're lost!
-        </Typography>
-        <Button component={Link} to="/" variant="contained" color="primary">
-          Go to Home
-        </Button>
-      </animated.div>
+      <Typography variant="h1" color="textPrimary" gutterBottom>
+        404 Not Found
+      </Typography>
+      <Typography variant="h4" color="textSecondary" paragraph>
+        Oops, it looks like you're lost!
+      </Typography>
+      <Button component={Link} to="/" variant="contained" color="primary">
+        Go to Home
+      </Button>
     </Container>
   );
 };
